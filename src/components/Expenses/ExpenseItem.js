1790ab3e5e52c0faeff0cbf4b71300fbcba0e15a
Ujjwal Kamilya <ujjwalkamilya970@gmail.com>
2022-05-20 21:06:04 +0530

    added Expenses, Card component plus css for both component and changed folder structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,10 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
+import Card from '../UI/Card'
 const ExpenseItem = (props) => {
   
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <div>
         <ExpenseDate date ={props.date}/>
       </div>
@@ -11,7 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{props.name}</h2>
       </div>
       <div className='expense-item__price'>{props.amount}</div>
-    </div>
+    </Card>
   )
 }
 
