0534149a8a156335748888e0cd945599469beed0
Ujjwal Kamilya <ujjwalkamilya970@gmail.com>
2022-05-20 20:25:56 +0530

    added ExpenseItem.css, ExpenseDate component and dummy expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-const ExpenseItem = () => {
-  return <h2>ExpenseItem</h2>
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+const ExpenseItem = (props) => {
+  
+  return (
+    <div className='expense-item'>
+      <div>
+        <ExpenseDate date ={props.date}/>
+      </div>
+      <div className='expense-item__description'>
+        <h2>{props.name}</h2>
+      </div>
+      <div className='expense-item__price'>{props.amount}</div>
+    </div>
+  )
 }
 
 export default ExpenseItem
